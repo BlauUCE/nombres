348eb640b3a91086c441dad9481f75486c6965ba
baidrovoc <baidrovoc@gmail.com>
2021-10-12 16:14:36 -0500

    menus Byron

--- menus-es_EC.js
+++ menus-es_EC.js
@@ -6,7 +6,12 @@
 			"MNU_ANB_EVAL": "Alejandro Naranjo",
 			"MNU_ANB_EVAL_BLI_GRID": "Ingresar Producto",
 			"MNU_ANB_EVAL_BLI_GRID_L": "Listar Productos",
-			"MNU_ANB_EVAL_BLI_GRID_CLI": "Buscar cliente"
+			"MNU_ANB_EVAL_BLI_GRID_CLI": "Buscar cliente",
+			"MNU_BIC_BVDA_":"Bienvenida Byron Andres Idrovo",
+			"BYRON_IDROVO":"Byron Idrovo",
+			"MNU_BIC_CREAPRO":"Crear Producto",
+			"MNU_BIC_LISTPRO":"Listar Productos",
+			"MNU_BIC_BUSCLI":"Buscar Cliente"
         }
     }
 }
