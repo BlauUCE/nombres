b930a5d15f0fe9b155f12cfb49482466c3537f19
NaranjoBrito <nb_correo@hotmail.com>
2021-10-12 15:59:47 -0500

    creacion de archivo menus-es_EC.json

---
+++ menus-es_EC.js
@@ -0,0 +1,12 @@
+{
+    "COMMONS": {
+        "MENU": {
+            "MNU_ANB_BVDA":"Bienvenido Alejandro Naranjo",
+			"MNU_SMCMC":"Formularios Semillero Sep",
+			"MNU_ANB_EVAL": "Alejandro Naranjo",
+			"MNU_ANB_EVAL_BLI_GRID": "Ingresar Producto",
+			"MNU_ANB_EVAL_BLI_GRID_L": "Listar Productos",
+			"MNU_ANB_EVAL_BLI_GRID_CLI": "Buscar cliente"
+        }
+    }
+}
